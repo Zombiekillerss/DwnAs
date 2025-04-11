d74418cf5cad242f3b6809d8d6c3987828d4cf84
alexandr <alexandr.2100@mail.ru>
2025-04-11 13:58:00 +0300

    test commit

--- app/src/main/java/com/example/dwnas/ManifestActivity.kt
+++ app/src/main/java/com/example/dwnas/ManifestActivity.kt
@@ -3,6 +3,8 @@ package com.example.dwnas
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
@@ -19,6 +21,8 @@ import com.example.dwnas.database.ListItemLink
 import com.example.dwnas.database.ListItemManifests
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 
 class ManifestActivity : ComponentActivity(), ItemManifestAdapter.Listener,
@@ -26,55 +30,21 @@ class ManifestActivity : ComponentActivity(), ItemManifestAdapter.Listener,
     private lateinit var itemLinkAdapter: ItemLinkAdapter
     private lateinit var itemManifestAdapter: ItemManifestAdapter
     private lateinit var dB: DBRequestMaker
-    private lateinit var  webView: WebView
-    private lateinit var  bSaveLink: Button
-    private lateinit var  bDeleteLink: Button
-    private lateinit var  bHandleLinks: Button
-    private lateinit var  etLink: EditText
-    private lateinit var  etName: EditText
+    private lateinit var webView: WebView
+    private lateinit var bSaveLink: Button
+    private lateinit var bDeleteLink: Button
+    private lateinit var bHandleLinks: Button
+    private lateinit var etLink: EditText
+    private lateinit var etName: EditText
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.get_manifest_link)
         initViews()
-
         initRcViews()
         updateList()
-        webView.getSettings().javaScriptEnabled = true
         Log.d("myresult request", "test")
-        webView.webViewClient = object : WebViewClient() {
-            override fun onPageFinished(view: WebView, url: String) {
-                super.onPageFinished(view, url)
-                webView.evaluateJavascript(
-                    "(function() { return document.documentElement.outerHTML; })();"
-                ) { html ->
-                    lifecycleScope.launch {
-                        val unescapedHtml = html
-                            .replace("\\u003C", "<")
-                            .replace("\\u003E", ">")
-                            .replace("\\\"", "\"")
-                            .removeSurrounding("\"")
-                        val scriptContents = extractScriptContents(unescapedHtml)
-
-                        // Ищем функцию среди содержимого script-тегов
-                        val targetFunction = findFunctionInScripts(scriptContents, "(function()")
-
-
-                        if (targetFunction != null) {
-                            Log.d("myresult request", targetFunction.toString())
-                            copyToClipboard(this@ManifestActivity, targetFunction[1])
-                            runOnUiThread {
-
-                            }
-                            // Используйте найденную функцию
-                        } else {
-                            Log.d("myresult request", "Функция не найдена")
-                        }
-                    }
-                }
-            }
-        }
 
         bSaveLink.setOnClickListener {
             val item = ListItemLink(name = etName.text.toString(), link = etLink.text.toString())
@@ -88,55 +58,17 @@ class ManifestActivity : ComponentActivity(), ItemManifestAdapter.Listener,
         bDeleteLink.setOnClickListener {
             etLink.text.clear()
         }
+
         bHandleLinks.setOnClickListener {
-            webView.loadUrl(etLink.text.toString())
-        }
-    }
+            val list = itemLinkAdapter.currentList
+            lifecycleScope.launch {
+                for (item in list) {
+                    loadUrlAndWait(item.link)
 
-
-    fun extractScriptContents(html: String): List<String> {
-        val scriptContents = mutableListOf<String>()
-        val scriptRegex = "<script[^>]*>(.*?)</script>".toRegex(RegexOption.DOT_MATCHES_ALL)
-
-        scriptRegex.findAll(html).forEach { matchResult ->
-            val content = matchResult.groupValues[1]
-            if (content.isNotBlank()) {
-                scriptContents.add(content)
-            }
-        }
-
-        return scriptContents
-    }
-
-    // Функция для поиска конкретной функции в содержимом script-тегов
-    fun findFunctionInScripts(scripts: List<String>, functionStart: String): List<String>? {
-        for (script in scripts) {
-            val startIndex = script.indexOf(functionStart)
-            if (startIndex == -1) continue
-
-            var openBraces = 0
-            var currentIndex = startIndex
-
-            while (currentIndex < script.length) {
-                when (script[currentIndex]) {
-                    '{' -> openBraces++
-                    '}' -> {
-                        openBraces--
-                        if (openBraces == 0) {
-                            // Нашли закрывающую скобку функции
-                            var str = script.substring(startIndex, currentIndex + 1)
-                            if (str.contains("\"streams\"")) {
-                                str = str.substring(str.indexOf("\"streams\""))
-                                str = str.substring(str.indexOf('[') + 1, str.indexOf(']'))
-                                return str.split(',')
-                            }
-                        }
-                    }
                 }
-                currentIndex++
             }
+
         }
-        return null
     }
 
     override fun onClickSave(manifest: ListItemManifests) {
@@ -155,7 +87,7 @@ class ManifestActivity : ComponentActivity(), ItemManifestAdapter.Listener,
     }
 
     override fun onClickDelete(link: ListItemLink) {
-        lifecycleScope.launch {
+        lifecycleScope.launch(Dispatchers.IO) {
             dB.deleteLink(this@ManifestActivity, link)
             updateList()
         }
@@ -189,13 +121,106 @@ class ManifestActivity : ComponentActivity(), ItemManifestAdapter.Listener,
         rcViewListLinks.adapter = itemLinkAdapter
     }
 
-    private fun initViews(){
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews() {
         webView = findViewById(R.id.wv)
+        webView.getSettings().javaScriptEnabled = true
         bSaveLink = findViewById(R.id.bSaveLink)
         bDeleteLink = findViewById(R.id.bDelLink)
         bHandleLinks = findViewById(R.id.bGetMpdLinks)
         etLink = findViewById(R.id.etLink)
         etName = findViewById(R.id.etName)
         dB = ViewModelProvider(this@ManifestActivity)[DBRequestMaker::class]
+    }
+
+    private fun extractScriptContents(html: String): List<String> {
+        val scriptContents = mutableListOf<String>()
+        val scriptRegex = "<script[^>]*>(.*?)</script>".toRegex(RegexOption.DOT_MATCHES_ALL)
+
+        scriptRegex.findAll(html).forEach { matchResult ->
+            val content = matchResult.groupValues[1]
+            if (content.isNotBlank()) {
+                scriptContents.add(content)
+            }
+        }
+
+        return scriptContents
+    }
+
+    private fun findFunctionInScripts(scripts: List<String>): List<String>? {
+        for (script in scripts) {
+            val startIndex = script.indexOf("(function()")
+            if (startIndex == -1) continue
+
+            var openBraces = 0
+            var currentIndex = startIndex
+
+            while (currentIndex < script.length) {
+                when (script[currentIndex]) {
+                    '{' -> openBraces++
+                    '}' -> {
+                        openBraces--
+                        if (openBraces == 0) {
+                            var str = script.substring(startIndex, currentIndex + 1)
+                            if (str.contains("\"streams\"")) {
+                                str = str.substring(str.indexOf("\"streams\""))
+                                str = str.substring(str.indexOf('[') + 1, str.indexOf(']'))
+                                return str.split(',')
+                            }
+                        }
+                    }
+                }
+                currentIndex++
+            }
+        }
+        return null
+    }
+
+    private suspend fun loadUrlAndWait(url: String): String = suspendCancellableCoroutine { continuation ->
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                webView.evaluateJavascript(
+                    "(function() { return document.documentElement.outerHTML; })();"
+                ) { html ->
+                    lifecycleScope.launch {
+                        val unescapedHtml = html
+                            .replace("\\u003C", "<")
+                            .replace("\\u003E", ">")
+                            .replace("\\\"", "\"")
+                            .removeSurrounding("\"")
+                        val scriptContents = extractScriptContents(unescapedHtml)
+
+                        val targetFunction = findFunctionInScripts(scriptContents)
+
+
+                        if (targetFunction != null) {
+                            Log.d("myresult request", targetFunction.toString())
+                            runOnUiThread {
+
+                            }
+                        } else {
+                            Log.d("myresult request", "Функция не найдена")
+                        }
+                    }
+                }
+                continuation.resume("+")
+            }
+
+            override fun onReceivedError(
+                view: WebView?,
+                request: WebResourceRequest?,
+                error: WebResourceError?
+            ) {
+                super.onReceivedError(view, request, error)
+                continuation.resume("-")
+            }
+        }
+
+        webView.loadUrl(url)
+
+        continuation.invokeOnCancellation {
+            webView.webViewClient = WebViewClient()
+        }
     }
 }
